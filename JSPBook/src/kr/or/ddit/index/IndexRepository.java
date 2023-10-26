cbe6333200a461c54473d845b121fa5077279aca
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-26 12:09:52 +0900

    1026

--- JSPBook/src/kr/or/ddit/index/IndexRepository.java
+++ JSPBook/src/kr/or/ddit/index/IndexRepository.java
@@ -17,9 +17,49 @@ public class IndexRepository {
 	
 	private IndexRepository() {
 		//챕터 정보를
+		String[] chapters = {
+			"CH01","CH02","CH03","CH04","CH05","CH06","CH07",
+			"CH08","CH10","CH11","CH12","CH13","CH14","CH17",
+		};
 		//각 챕터의 제목들
+		String[] titles = {
+			"CH01. JSP 개요",	"CH02. 스크립트 태그","CH03. 디렉티브 태그","CH04. 액션태그",	
+			"CH05. 내장객체","CH06. 폼 태그",	"CH07. 파일업로드",	"CH08. 유효성 검사",	
+			"CH10. 시큐리티",	"CH11. 예외처리",	"CH12. 필터",	"CH13. 세션",	"CH14. 쿠키",	
+			"CH17. 태그 라이브러리"
+		};
 		//각 챕터의 이동 URL
+		String[] chapterMoveUrls = {
+				"/welcome.jsp","/ch02/scripting.jsp","/ch03/","/ch04/",
+				"/ch05/","/ch06/","/ch07/","/ch08/",
+				"/ch10/","/ch11/","/ch12/","/ch13/",
+				"/ch14/","/ch17/"				
+		};
 		//각 챕터의 소스 파일명들
+		String[][] sourceArr = {
+				{							//ch01
+					"/welcome.jsp"			
+				},
+				{							//ch02
+					"scripting.jsp",
+					"declaration01.jsp"
+					
+				},
+				{""},						//ch03
+				{""},						//ch04
+				{""},						//ch05
+				{""},						//ch06
+				{""},						//ch07
+				{""},						//ch08
+				{""},						//ch10
+				{""},						//ch11
+				{""},						//ch12
+				{""},						//ch13
+				{""},						//ch14
+				{""},						//ch17
+
+				
+		};
 		
 		//최종 indexList에 추가
 		
